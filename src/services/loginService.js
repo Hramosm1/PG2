2ef360384e7075c700f7c8eb9942e038dd538952
Hramosm1 <hectorzramos@gmail.com>
2023-08-30 11:59:00 -0600

    Tiempo expira token

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -22,7 +22,7 @@ const loginUser = async (identifier, password) => {
       throw new Error('Contraseña incorrecta');
     }
 
-    const token = jwt.sign({ id: user.id_usuario }, process.env.SECRET_KEY, { expiresIn: '2h' });
+    const token = jwt.sign({ id: user.id_usuario }, process.env.SECRET_KEY, { expiresIn: '24h' });
 
     return token;
   } catch (error) {
